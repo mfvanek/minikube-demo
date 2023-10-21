465032b02796dbeb1aec52f154f5c2e07acde195
Ivan Vakhrushev <mfvanek@gmail.com>
2023-10-21 17:55:12 +0400

    Added preStop hook (#50)

--- src/main/java/io/github/mfvanek/minikubedemo/controller/WorkloadController.java
+++ src/main/java/io/github/mfvanek/minikubedemo/controller/WorkloadController.java
@@ -1,8 +1,5 @@
 package io.github.mfvanek.minikubedemo.controller;
 
-import java.time.OffsetDateTime;
-import java.util.concurrent.TimeUnit;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +7,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.OffsetDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/workload")
 public class WorkloadController {
 
-    @SneakyThrows
+    private final AtomicLong counter = new AtomicLong();
+
     @GetMapping("/now")
     public ResponseEntity<OffsetDateTime> nowWithDelay(@RequestParam("delay") final long delayInMilliseconds) {
-        if (delayInMilliseconds >= 0) {
-            TimeUnit.MILLISECONDS.sleep(delayInMilliseconds);
-        } else {
-            log.warn("Skipping negative delay value {}", delayInMilliseconds);
+        final long requestNum = counter.incrementAndGet();
+        try {
+            log.info("executing nowWithDelay {}", requestNum);
+            if (delayInMilliseconds >= 0) {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(delayInMilliseconds);
+                } catch (InterruptedException e) {
+                    log.warn("nowWithDelay was interrupted", e);
+                    Thread.currentThread().interrupt();
+                }
+            } else {
+                log.warn("Skipping negative delay value {}", delayInMilliseconds);
+            }
+            return ResponseEntity.ok(OffsetDateTime.now());
+        } finally {
+            log.info("finishing execution of nowWithDelay {}", requestNum);
         }
-        return ResponseEntity.ok(OffsetDateTime.now());
     }
 }
