b0b248ecbdf58743fed81cd6ae1a825e1d7bec3a
Ivan Vakhrushev <mfvanek@gmail.com>
2023-12-24 19:45:57 +0400

    Add error prone

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,20 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import net.ltgt.gradle.errorprone.errorprone
 
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.2.0"
+    id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
     id("com.bmuschko.docker-java-application") version "9.4.0"
     id("com.google.osdetector") version "1.7.3"
     id("io.freefair.lombok") version "8.4"
     id("com.github.ben-manes.versions") version "0.50.0"
-    id("io.gatling.gradle") version "3.10.0"
+    id("io.gatling.gradle") version "3.10.3"
+    id("net.ltgt.errorprone") version "3.1.0"
 }
 
 group = "io.github.mfvanek"
-version = "0.2.3"
+version = "0.2.4"
 
 java {
     toolchain {
@@ -21,6 +23,10 @@ java {
 }
 tasks.withType<JavaCompile>().configureEach {
     options.compilerArgs.add("-parameters")
+    options.errorprone {
+        disableWarningsInGeneratedCode.set(true)
+        disable("Slf4jLoggerShouldBeNonStatic")
+    }
 }
 
 repositories {
@@ -28,19 +34,22 @@ repositories {
 }
 
 dependencies {
-    implementation("io.micrometer:micrometer-registry-prometheus:1.12.1")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.google.code.findbugs:jsr305:3.0.2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.assertj:assertj-core")
 
     // https://github.com/netty/netty/issues/11020
     if (osdetector.arch == "aarch_64") {
         testImplementation("io.netty:netty-all:4.1.104.Final")
     }
+
+    errorprone("com.google.errorprone:error_prone_core:2.24.0")
+    errorprone("jp.skypencil.errorprone.slf4j:errorprone-slf4j:0.1.21")
 }
 
 tasks.withType<Test>().configureEach {
